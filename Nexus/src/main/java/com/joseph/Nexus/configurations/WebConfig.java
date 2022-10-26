11a6e24d824a1ee10f91b0e972db7a3f5b714620
JT0311 <joseph03walker@gmail.com>
2022-10-25 21:32:58 -0400

    updated configuration

--- Nexus/src/main/java/com/joseph/Nexus/configurations/WebConfig.java
+++ Nexus/src/main/java/com/joseph/Nexus/configurations/WebConfig.java
@@ -1,17 +1,17 @@
-package com.joseph.Nexus.configurations;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-    }
-}
+//package com.joseph.Nexus.configurations;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/static/**")
+//                .addResourceLocations("/static/index.html");
+//    }
+//}
