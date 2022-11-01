c2b892460b9c4a30ca976d66edcb1bc9306a210f
JT0311 <joseph03walker@gmail.com>
2022-10-31 22:15:47 -0400

    updated Dockerfile

--- Nexus/src/test/java/com/joseph/Nexus/NexusApplicationTests.java
+++ Nexus/src/test/java/com/joseph/Nexus/NexusApplicationTests.java
@@ -1,11 +1,13 @@
 package com.joseph.Nexus;
 
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NexusApplicationTests {
 
+	@IgnoreForBinding
 	@Test
 	void contextLoads() {
 	}
