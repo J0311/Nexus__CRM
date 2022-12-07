63dff9a650b5855be0f448c7e595ed3a54f8ae9e
JT0311 <joseph03walker@gmail.com>
2022-12-06 21:56:04 -0500

    services/business

--- Nexus/src/main/java/com/joseph/Nexus/repos/BusinessRepo.java
+++ Nexus/src/main/java/com/joseph/Nexus/repos/BusinessRepo.java
@@ -21,4 +21,16 @@ public interface BusinessRepo extends JpaRepository<Business, Integer> {
 
     @Query(value = "SELECT * FROM product_reviews WHERE business_id = :businessId", nativeQuery = true)
     List<Business> findAllByBusinessId(@Param("businessId") int businessId);
+
+//    List<Business> searchByName(String kw);
+    /**
+     * Returns a list of all businesses in the database that are associated with the
+     * given keyword.
+     *
+     * @param keyword - the keyword to search for
+     * @return a list of all businesses whose name contains the given keyword
+     */
+
+    @Query("SELECT p FROM Product p WHERE UPPER(name) LIKE CONCAT('%',UPPER(:keyword),'%')")
+    List<Business> searchByName(@Param("keyword") String keyword);
 }
--- Nexus/src/main/java/com/joseph/Nexus/services/BusinessService.java
+++ Nexus/src/main/java/com/joseph/Nexus/services/BusinessService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BusinessService {
@@ -26,8 +27,51 @@ public class BusinessService {
      * empty list is returned.
      * @return a list of all businesses in the database
      */
+
     public List<Business> findAll() {
         return businessRepo.findAll();
+    }
+
+    /**
+     * Returns the business with the given business id if it exists in the database.
+     *
+     * @param business_Id - the id of the business to retrieve
+     * @return an optional containing the business with the given id if it exists
+     */
+
+    public Optional<Business> findById(int business_Id) {
+        return businessRepo.findById(business_Id);
+    }
+
+    /**
+     * Saves the given business to the database.
+     *
+     * @param business - the business to save
+     * @return the saved business
+     */
+
+    public Business save(Business business) {
+        return businessRepo.save(business);
+    }
+
+    /**
+     * Deletes the business with the given id from the database.
+     *
+     * @param business_Id - the id of the business to delete
+     */
+
+    public void delete(int business_Id) {
+        businessRepo.deleteById(business_Id);
+    }
+
+    /**
+     * Returns a list of businesses with the given keyword in the name.
+     *
+     * @param keyword - the keyword to search for
+     * @return a list of businesses with the given keyword in the name
+     */
+    public List<Business> searchByName(String keyword) {
+        return businessRepo.searchByName(keyword);
     }
 
 }
