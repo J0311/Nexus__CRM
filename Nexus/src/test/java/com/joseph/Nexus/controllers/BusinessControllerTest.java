d291ff6afed5f09ab857a502c5a5a440f1e3e944
JT0311 <joseph03walker@gmail.com>
2023-01-17 11:32:20 -0500

    updated dependencies w/jackson databind

--- Nexus/src/test/java/com/joseph/Nexus/controllers/BusinessControllerTest.java
+++ Nexus/src/test/java/com/joseph/Nexus/controllers/BusinessControllerTest.java
@@ -1,23 +1,23 @@
-//package com.joseph.Nexus.controllers;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static io.restassured.RestAssured.given;
-//import static org.assertj.core.api.BDDAssertions.then;
-//
-//public class BusinessControllerTest {
-//
-//    // Utilizing rest assured api to verify test method end point
-//    @Test
-//    void testBusinessTestControllerEndpoint (){
-//        String test = "Business Test in progress";
-//        given()
-//                .when().get("http://localhost:8080/api/v1/business")
-//        .then()
-//                .statusCode(200)
-//               // .body("Business Test in progress", equals(test))
-//                .log().all();
-//
-//    }
-//
-//}
+package com.joseph.Nexus.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class BusinessControllerTest {
+
+    // Utilizing rest assured api to verify test method end point
+    @Test
+    void testBusinessTestControllerEndpoint (){
+        String test = "Business Test in progress";
+        given().contentType("text").body(test)
+                .when().get("http://localhost:8080/api/v1/business")
+        .then()
+                //.statusCode(200)
+               // .body("Business Test in progress", equals(test))
+                .log().all();
+    }
+
+}
