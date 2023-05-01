d13eec04b86bc9c75ab28a2e59738af8f09284ba
JT0311 <joseph03walker@gmail.com>
2023-05-01 16:20:44 -0400

    customer controller class added

--- Nexus/src/main/java/com/joseph/Nexus/controllers/CustomerController.java
+++ Nexus/src/main/java/com/joseph/Nexus/controllers/CustomerController.java
@@ -1,10 +1,53 @@
 package com.joseph.Nexus.controllers;
 
+import com.joseph.Nexus.models.Customer;
+import com.joseph.Nexus.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/customer")
+@RequestMapping("/api/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customeres = customerService.getAllCustomers();
+        return new ResponseEntity<>(customeres, HttpStatus.OK);
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable int customerId) {
+        Optional<Customer> customer = customerService.getCustomerById(customerId);
+        if (customer.isPresent()) {
+            return new ResponseEntity<>(customer.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        Customer newCustomer = customerService.addCustomer(customer);
+        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable int customerId, @RequestBody Customer customer) {
+        customerService.updateCustomer(customerId, customer);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable int customerId) {
+        customerService.deleteCustomer(customerId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
