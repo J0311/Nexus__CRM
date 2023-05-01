da14d553469a364e376e6ccffabc08a10cc98c5c
JT0311 <joseph03walker@gmail.com>
2023-05-01 16:29:45 -0400

    contract services

--- Nexus/src/main/java/com/joseph/Nexus/services/ContractService.java
+++ Nexus/src/main/java/com/joseph/Nexus/services/ContractService.java
@@ -1,7 +1,36 @@
 package com.joseph.Nexus.services;
 
+import com.joseph.Nexus.models.Contract;
+import com.joseph.Nexus.repos.ContractRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContractService {
+
+    private final ContractRepo contractRepository;
+
+    @Autowired
+    public ContractService(ContractRepo contractRepository) {
+        this.contractRepository = contractRepository;
+    }
+
+    public List<Contract> getAllContracts() {
+        return contractRepository.findAll();
+    }
+
+    public Optional<Contract> getContractById(int id) {
+        return contractRepository.findById(id);
+    }
+
+    public Contract saveContract(Contract contract) {
+        return contractRepository.save(contract);
+    }
+
+    public void deleteContract(int id) {
+        contractRepository.deleteById(id);
+    }
 }
