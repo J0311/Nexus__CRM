4a843c06d9e7de11f2214fbe34aaee6e0cd0ffdf
JT0311 <joseph03walker@gmail.com>
2023-05-01 16:09:50 -0400

    customer services class

--- Nexus/src/main/java/com/joseph/Nexus/services/CustomerService.java
+++ Nexus/src/main/java/com/joseph/Nexus/services/CustomerService.java
@@ -1,7 +1,43 @@
 package com.joseph.Nexus.services;
 
+import com.joseph.Nexus.models.Customer;
+import com.joseph.Nexus.repos.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    public List<Customer> getAllCustomers() {
+        return customerRepo.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(int customerId) {
+        return customerRepo.findById(customerId);
+    }
+
+    public Customer addCustomer(Customer customer) {
+        return customerRepo.save(customer);
+    }
+
+    public void updateCustomer(int customerId, Customer customer) {
+        Optional<Customer> existingCustomer = customerRepo.findById(customerId);
+        if (existingCustomer.isPresent()) {
+            customer.setCustomerId(customerId);
+            customerRepo.save(customer);
+        }
+    }
+
+    public void deleteCustomer(int customerId) {
+        Optional<Customer> customer = customerRepo.findById(customerId);
+        if (customer.isPresent()) {
+            customerRepo.deleteById(customerId);
+        }
+    }
 }
