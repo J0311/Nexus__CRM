bd1bf931815c6a0f0b6391d8ce05741f5a686d05
JT0311 <joseph03walker@gmail.com>
2023-05-18 12:12:40 -0400

    update

--- Nexus/src/main/java/com/joseph/Nexus/NexusApplication.java
+++ Nexus/src/main/java/com/joseph/Nexus/NexusApplication.java
@@ -1,6 +1,5 @@
 package com.joseph.Nexus;
 
-import com.joseph.Nexus.utils.Tweet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
